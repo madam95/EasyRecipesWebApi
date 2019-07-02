ed7a58986e34d6b58905a0a5d6cde9590500972e
Cyrus <m.adamkiewicz95@gmail.com>
2019-07-02 20:41:55 +0200

    Added seed data for the MeasurementUnit table

---
+++ EasyRecipesWebApi/Migrations/20190702184011_ModelBuilderSeedTest.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EasyRecipesWebApi.Migrations
+{
+    public partial class ModelBuilderSeedTest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "MeasurementUnit",
+                columns: new[] { "Id", "Unit" },
+                values: new object[,]
+                {
+                    { 1, "Gram" },
+                    { 2, "Millilitre" },
+                    { 3, "Piece" },
+                    { 4, "Spoon" },
+                    { 5, "Tablespoon" },
+                    { 6, "Pinch" },
+                    { 7, "Glass" },
+                    { 8, "None" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "MeasurementUnit",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "MeasurementUnit",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "MeasurementUnit",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "MeasurementUnit",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "MeasurementUnit",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "MeasurementUnit",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "MeasurementUnit",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "MeasurementUnit",
+                keyColumn: "Id",
+                keyValue: 8);
+        }
+    }
+}
--- EasyRecipesWebApi/Models/EasyRecipesContext.cs
+++ EasyRecipesWebApi/Models/EasyRecipesContext.cs
@@ -17,9 +17,17 @@ namespace EasyRecipesWebApi.Models
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<MeasurementUnit>().HasData(
+                new {Id = 1, Unit = "Gram"},
+                new {Id = 2, Unit = "Millilitre"},
+                new {Id = 3, Unit = "Piece"},
+                new {Id = 4, Unit = "Spoon"},
+                new {Id = 5, Unit = "Tablespoon"},
+                new {Id = 6, Unit = "Pinch"},
+                new {Id = 7, Unit = "Glass"},
+                new {Id = 8, Unit = "None"});
         }
 
         public DbSet<Ingredient> Ingredients { get; set; }
-        
     }
 }
