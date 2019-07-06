1ef7bc7046e60f4b904c75068dd9069d37f1813a
Cyrus <m.adamkiewicz95@gmail.com>
2019-07-06 13:44:47 +0200

    Adding folders and intrefaces in order to increase abstraction level

---
+++ EasyRecipesWebApi/Repositories/IIngredientRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EasyRecipesWebApi.Models;
+
+namespace EasyRecipesWebApi.Repositories
+{
+    public interface IIngredientRepository
+    {
+        Task<IEnumerable<Ingredient>> GetAllIngredientsAsync();
+    }
+}
---
+++ EasyRecipesWebApi/Services/IngredientService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using EasyRecipesWebApi.Models;
+using EasyRecipesWebApi.Repositories;
+
+namespace EasyRecipesWebApi.Services
+{
+    public class IngredientService : IIngredientService
+    {
+        private readonly IIngredientRepository _ingredientRepository;
+
+        public IngredientService(IIngredientRepository ingredientRepository)
+        {
+            _ingredientRepository = ingredientRepository;
+        }
+
+        public async Task<IEnumerable<Ingredient>> GetAllIngredientsAsync()
+        {
+            return await _ingredientRepository.GetAllIngredientsAsync();
+        }
+    }
+}
